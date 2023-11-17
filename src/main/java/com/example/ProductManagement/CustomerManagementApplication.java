28aa6fc7488200c2274f2b0c4dc49b40585fb023
unknown <deepak.appala@pec.edu>
2023-11-18 01:35:26 +0530

    chage14

--- src/main/java/com/example/ProductManagement/CustomerManagementApplication.java
+++ src/main/java/com/example/ProductManagement/CustomerManagementApplication.java
@@ -3,9 +3,11 @@ package com.example.ProductManagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@ComponentScan("com.example.ProductManagement.repository")
 public class CustomerManagementApplication {
 
 	public static void main(String[] args) {
